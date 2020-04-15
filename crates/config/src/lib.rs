5d8cabe57b32857729a66090b1b785c9e133bdd8
katand <katandps@gmail.com>
2020-04-15 13:28:58 +0900

    [feature] Configのシングルトン化

--- crates/config/src/lib.rs
+++ crates/config/src/lib.rs
@@ -1,68 +1,130 @@
+#[macro_use]
+extern crate lazy_static;
 use serde::{Deserialize, Serialize};
-use std::fs;
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Config {
-    timestamp: Option<i32>,
-    local_cache_url: String,
-    score_db_url: String,
-    songdata_db_url: String,
-    scorelog_db_url: String,
-    table_urls: Option<Vec<String>>,
-    table_index: Option<usize>,
-    recommend_song_number: Option<usize>,
-    coloring_table: Option<bool>,
-    slack_bot_token: Option<String>,
-    slack_channel: Option<String>,
-    slack_file_name: Option<String>,
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum Config {
+    Config(Cfg),
+    Dummy,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Cfg {
+    pub timestamp: Option<i32>,
+    pub local_cache_url: String,
+    pub score_db_url: String,
+    pub songdata_db_url: String,
+    pub scorelog_db_url: String,
+    pub table_urls: Option<Vec<String>>,
+    pub table_index: Option<usize>,
+    pub recommend_song_number: Option<usize>,
+    pub coloring_table: Option<bool>,
+    pub slack_bot_token: Option<String>,
+    pub slack_channel: Option<String>,
+    pub slack_file_name: Option<String>,
 }
 
 impl Config {
     pub fn score_db_url(&self) -> String {
-        self.score_db_url.clone()
+        match self {
+            Config::Config(cfg) => cfg.score_db_url.clone(),
+            _ => "score_db_url".into(),
+        }
     }
     pub fn song_db_url(&self) -> String {
-        self.songdata_db_url.clone()
+        match self {
+            Config::Config(cfg) => cfg.songdata_db_url.clone(),
+            _ => "song_db_url".into(),
+        }
     }
     pub fn scorelog_db_url(&self) -> String {
-        self.scorelog_db_url.clone()
+        match self {
+            Config::Config(cfg) => cfg.scorelog_db_url.clone(),
+            _ => "scorelog_db_url".into(),
+        }
     }
     pub fn local_cache_url(&self) -> String {
-        self.local_cache_url.clone()
+        match self {
+            Config::Config(cfg) => cfg.local_cache_url.clone(),
+            _ => "local_cache_url".into(),
+        }
     }
     pub fn timestamp(&self) -> i32 {
-        self.timestamp.unwrap_or(1800000000)
+        match self {
+            Config::Config(cfg) => cfg.timestamp.unwrap_or(1800000000),
+            _ => 1800000000,
+        }
     }
     pub fn recommend_song_number(&self) -> usize {
-        self.recommend_song_number.unwrap_or(3)
+        match self {
+            Config::Config(cfg) => cfg.recommend_song_number.unwrap_or(3),
+            _ => 3,
+        }
     }
     pub fn table_urls(&self) -> Vec<String> {
-        self.table_urls.clone().unwrap_or(Vec::new())
+        match self {
+            Config::Config(cfg) => cfg.table_urls.clone().unwrap_or(Vec::new()),
+            _ => vec!["table_url".into()],
+        }
     }
     pub fn table_index(&self) -> usize {
-        self.table_index.unwrap_or(0)
+        match self {
+            Config::Config(cfg) => cfg.table_index.unwrap_or(0),
+            _ => 0,
+        }
     }
     pub fn coloring_table(&self) -> bool {
-        self.coloring_table.unwrap_or(true)
+        match self {
+            Config::Config(cfg) => cfg.coloring_table.unwrap_or(true),
+            _ => true,
+        }
     }
     pub fn slack_bot_token(&self) -> String {
-        self.slack_bot_token
-            .clone()
-            .expect("Slack bot token is not configure.")
+        match self {
+            Config::Config(cfg) => cfg
+                .slack_bot_token
+                .clone()
+                .expect("Slack bot token is not configure."),
+            _ => "slack_bot_token bot token".into(),
+        }
     }
     pub fn slack_channel(&self) -> String {
-        self.slack_channel
-            .clone()
-            .expect("Slack channel is not configure.")
+        match self {
+            Config::Config(cfg) => cfg
+                .slack_channel
+                .clone()
+                .expect("Slack channel is not configure."),
+            _ => "slack_channel".into(),
+        }
     }
     pub fn slack_file_name(&self) -> String {
-        self.slack_file_name
-            .clone()
-            .expect("Slack file name is not configure")
+        match self {
+            Config::Config(cfg) => cfg
+                .slack_file_name
+                .clone()
+                .expect("Slack file name is not configure"),
+            _ => "slack_file_name".into(),
+        }
     }
 }
 
 pub fn config() -> Config {
+    (*self::CONFIG).clone()
+}
+
+lazy_static! {
+    pub static ref CONFIG: Config = cfg();
+}
+
+#[cfg(not(test))]
+use std::fs;
+#[cfg(not(test))]
+fn cfg() -> Config {
     let file = fs::read_to_string("config.toml").unwrap();
-    toml::from_str(&file).unwrap()
+    Config::Config(toml::from_str(&file).unwrap())
+}
+
+#[cfg(test)]
+fn cfg() -> Config {
+    Config::Dummy
 }
--- crates/model/src/scored_table.rs
+++ crates/model/src/scored_table.rs
@@ -1,4 +1,5 @@
 use crate::*;
+use std::cmp::Ordering;
 use std::fmt;
 
 pub struct ScoredTable {
@@ -11,7 +12,7 @@ impl ScoredTable {
     }
     pub fn recent_updated(&self) -> ScoredTable {
         let mut vec: Vec<ScoredChart> = self.charts.iter().cloned().collect();
-        vec.sort_by(|a, b| a.score.cmp(&b.score));
+        vec.sort_by(ScoredChart::cmp);
         ScoredTable::new(
             vec.iter()
                 .take(config::config().recommend_song_number())
@@ -31,16 +32,44 @@ impl fmt::Display for ScoredTable {
     }
 }
 
+trait ScoreCmp {
+    fn cmp(&self, other: &Self) -> Ordering;
+}
+
 #[derive(Clone)]
-pub struct ScoredChart {
-    song_id: SongId,
-    chart: Chart,
-    score: Score,
+pub enum ScoredChart {
+    ScoredChart {
+        song_id: SongId,
+        chart: Chart,
+        score: Score,
+    },
+    Dummy(i32),
+}
+
+impl ScoreCmp for ScoredChart {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (
+                ScoredChart::ScoredChart {
+                    song_id: _,
+                    chart: _,
+                    score: self_score,
+                },
+                ScoredChart::ScoredChart {
+                    song_id: _,
+                    chart: _,
+                    score: other_score,
+                },
+            ) => self_score.cmp(&other_score),
+            (ScoredChart::Dummy(s), ScoredChart::Dummy(o)) => s.cmp(o),
+            _ => panic!("実体とDummyを比較しようとしました。"),
+        }
+    }
 }
 
 impl ScoredChart {
     pub fn new(song_id: SongId, chart: Chart, score: Score) -> ScoredChart {
-        ScoredChart {
+        ScoredChart::ScoredChart {
             song_id,
             chart,
             score,
@@ -50,6 +79,13 @@ impl ScoredChart {
 
 impl fmt::Display for ScoredChart {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}\n{}", self.chart, self.score)
+        match self {
+            ScoredChart::ScoredChart {
+                song_id: _,
+                chart,
+                score,
+            } => write!(f, "{}\n{}", chart, score),
+            _ => write!(f, "Dummy Object"),
+        }
     }
 }
