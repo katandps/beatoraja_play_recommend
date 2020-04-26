27744247e062b863791464bd3068862cf752bd2f
katand <katandps@gmail.com>
2020-04-27 07:08:55 +0900

    [bug-fix] 出力が長いとSlackに出力できない問題の修正

--- crates/send_slack/src/lib.rs
+++ crates/send_slack/src/lib.rs
@@ -1,15 +1,36 @@
+use reqwest::blocking::multipart::Form;
+use reqwest::blocking::Client;
+use serde_json::Value;
+use std::fs;
+use std::io::Write;
+
 pub fn send(channel: String, title: String, content: String) -> anyhow::Result<String> {
-    let res = reqwest::blocking::Client::new()
-        .get("https://slack.com/api/files.upload")
+    let mut f = fs::File::create("buf.txt").unwrap();
+    f.write_all(content.as_bytes()).unwrap();
+    let form = Form::new().file("file", "buf.txt").unwrap();
+    let res = Client::new()
+        .post("https://slack.com/api/files.upload")
+        .multipart(form)
         .query(&[
             ("token", config().slack_bot_token()),
             ("title", title),
             ("channels", channel),
             ("pretty", "1".into()),
-            ("content", content),
         ])
         .send()?;
-    Ok(format!("{:#?}", res))
+    let _ = fs::remove_file("buf.txt");
+    match res.text() {
+        Err(e) => Ok(String::from(format!("アップロード失敗:{:?}", e))),
+        Ok(result) => {
+            let v: Value = serde_json::from_str(result.as_str()).unwrap();
+
+            match v["ok"].as_bool() {
+                Some(true) => Ok(String::from(format!("アップロード完了"))),
+                Some(false) => Ok(String::from(format!("アップロード失敗:{}", v["error"]))),
+                None => Ok(String::from(format!("アップロード失敗:{}", v["error"]))),
+            }
+        }
+    }
 }
 
 fn config() -> config::Config {
@@ -25,6 +46,6 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        println! {"{}",send("テスト用".into(),"タイトル".into(), "内容".into()).unwrap()};
+        println! {"{}",send("テスト用".into(),"タイトル".into(), "内容内容内容".into()).unwrap()};
     }
 }
