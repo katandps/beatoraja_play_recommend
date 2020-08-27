0258671550786ff27a5d6669ea88d0b2fe84b6f5
katand <katandps@gmail.com>
2020-08-28 07:24:59 +0900

    [feature] 非同期にSlackへのメッセージ送信を行う機能を実装

--- crates/send_slack/src/lib.rs
+++ crates/send_slack/src/lib.rs
@@ -1,10 +1,11 @@
-use reqwest::blocking::multipart::Form;
-use reqwest::blocking::Client;
 use serde_json::Value;
 use std::fs;
 use std::io::Write;
 
 pub fn send(channel: String, title: String, content: String) -> anyhow::Result<String> {
+    use reqwest::blocking::multipart::Form;
+    use reqwest::blocking::Client;
+
     let mut f = fs::File::create("buf.txt").unwrap();
     f.write_all(content.as_bytes()).unwrap();
     let form = Form::new().file("file", "buf.txt").unwrap();
@@ -33,6 +34,35 @@ pub fn send(channel: String, title: String, content: String) -> anyhow::Result<S
     }
 }
 
+pub async fn send_async(channel: String, title: String, content: String) -> anyhow::Result<String> {
+    use reqwest::multipart::{Form, Part};
+    use reqwest::Client;
+
+    let fp = Part::text(content).file_name("buf.txt");
+
+    let form = Form::new().part("file", fp);
+    let res = Client::new()
+        .post("https://slack.com/api/files.upload")
+        .multipart(form)
+        .query(&[
+            ("token", config().slack_bot_token()),
+            ("title", title),
+            ("channels", channel),
+            ("pretty", "1".into()),
+        ])
+        .send()
+        .await?
+        .text()
+        .await?;
+    let v: Value = serde_json::from_str(res.as_str()).unwrap();
+
+    match v["ok"].as_bool() {
+        Some(true) => Ok(String::from(format!("アップロード完了"))),
+        Some(false) => Ok(String::from(format!("アップロード失敗:{}", v["error"]))),
+        None => Ok(String::from(format!("アップロード失敗:{}", v["error"]))),
+    }
+}
+
 fn config() -> config::Config {
     if cfg!(test) {
         config::Config::Dummy
@@ -47,5 +77,15 @@ mod tests {
     #[test]
     fn it_works() {
         println! {"{}",send("テスト用".into(),"タイトル".into(), "内容内容内容".into()).unwrap()};
+    }
+
+    #[tokio::test]
+    async fn async_fn_works() {
+        println!(
+            "{}",
+            send_async("ほげほげ".into(), "タイトル".into(), "内容".into())
+                .await
+                .unwrap()
+        );
     }
 }
