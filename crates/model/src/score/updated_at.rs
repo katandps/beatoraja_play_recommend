5cfd42314f4d9193cfe78268f1389650db64b81e
katand <katandps@gmail.com>
2020-11-27 18:30:36 +0900

    [feature] updated_atに文字列から生成する機能を実装

--- crates/model/src/score/updated_at.rs
+++ crates/model/src/score/updated_at.rs
@@ -12,10 +12,33 @@ impl UpdatedAt {
     pub fn from_timestamp(timestamp: i32) -> UpdatedAt {
         UpdatedAt(DateTime::from(Local.timestamp(timestamp as i64, 0)))
     }
+    pub fn from_str(str: &str) -> UpdatedAt {
+        match DateTime::parse_from_rfc3339(format!("{}T00:00:00+09:00", str).as_str()) {
+            Ok(d) => UpdatedAt(DateTime::from(d)),
+            _ => UpdatedAt(Local::now()),
+        }
+    }
 }
 
 impl fmt::Display for UpdatedAt {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.0.format("%Y-%m-%d %H:%M:%S"))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    pub fn test() {
+        assert_eq!(
+            "1992-11-20 00:00:00",
+            UpdatedAt::from_str("1992-11-20").to_string()
+        );
+        assert_eq!(
+            Local::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+            UpdatedAt::from_str("hogehoge").to_string()
+        );
     }
 }
