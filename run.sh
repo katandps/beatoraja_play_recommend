62477a788dfa1cfd085faf52c8082d83e96e444c
katand <katandps@gmail.com>
2021-01-08 13:11:24 +0900

    [feature] .envファイルをs3からダウンロードするように変更

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
   #!/usr/bin/env bash
 IMAGE=847110695266.dkr.ecr.ap-northeast-1.amazonaws.com/beatoraja_recommend_server
 mkdir -p files
+aws s3 cp s3://beatoraja-play-recommend-prod-env/.env .env
 
 echo "Starting service."
 $(aws ecr get-login --no-include-email)
