7788559bf0929eb6cea5133a2e31645ffff3285e
katand <katandps@gmail.com>
2021-01-11 04:33:21 +0900

    [refactor] db_poolのクローン回数を減らした

--- crates/server/src/filter.rs
+++ crates/server/src/filter.rs
@@ -11,7 +11,7 @@ pub fn with_db(
     db_pool: &MySqlPool,
 ) -> impl Filter<Extract = (MySQLClient,), Error = Infallible> + Clone {
     let db_pool = db_pool.clone();
-    warp::any().map(move || MySQLClient::new(db_pool.clone().get().unwrap()))
+    warp::any().map(move || MySQLClient::new(db_pool.get().unwrap()))
 }
 
 pub fn with_table(tables: Tables) -> impl Filter<Extract = (Tables,), Error = Infallible> + Clone {
