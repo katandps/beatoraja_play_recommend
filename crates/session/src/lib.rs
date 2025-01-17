026410f3bc0c7a9d5422ad07fad41392cc53dae5
katandps <katandps@gmail.com>
2025-01-17 19:21:19 +0900

    enable session key cache

--- crates/session/src/lib.rs
+++ crates/session/src/lib.rs
@@ -1,3 +1,5 @@
+use std::sync::OnceLock;
+
 use anyhow::Result;
 use chrono::{Duration, Utc};
 use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
@@ -39,25 +41,31 @@ pub fn verify_session_jwt(jwt: &str) -> Result<Claims> {
 
 #[allow(unused)]
 fn get_private_key() -> Result<EncodingKey> {
-    let private_key = if let Ok(key) = std::env::var("SESSION_JWT_PRIVATE_KEY") {
-        key
-    } else {
-        let path: String = std::env::var("SESSION_JWT_PRIVATE_KEY_PATH").unwrap();
-        std::fs::read_to_string(path)?
-    };
-    let result = EncodingKey::from_ec_pem(&private_key.into_bytes())?;
+    static PRIVATE_KEY: OnceLock<String> = OnceLock::new();
+    let private_key = PRIVATE_KEY.get_or_init(|| {
+        if let Ok(key) = std::env::var("SESSION_JWT_PRIVATE_KEY") {
+            key
+        } else {
+            let path: String = std::env::var("SESSION_JWT_PRIVATE_KEY_PATH").unwrap();
+            std::fs::read_to_string(path).unwrap()
+        }
+    });
+    let result = EncodingKey::from_ec_pem(private_key.as_bytes())?;
     Ok(result)
 }
 
 #[allow(unused)]
 fn get_public_key() -> Result<DecodingKey> {
-    let public_key = if let Ok(key) = std::env::var("SESSION_JWT_PUBLIC_KEY") {
-        key
-    } else {
-        let path: String = std::env::var("SESSION_JWT_PUBLIC_KEY_PATH").unwrap();
-        std::fs::read_to_string(path)?
-    };
-    let result = DecodingKey::from_ec_pem(&public_key.into_bytes())?;
+    static PUBLIC_KEY: OnceLock<String> = OnceLock::new();
+    let public_key = PUBLIC_KEY.get_or_init(|| {
+        if let Ok(key) = std::env::var("SESSION_JWT_PUBLIC_KEY") {
+            key
+        } else {
+            let path: String = std::env::var("SESSION_JWT_PUBLIC_KEY_PATH").unwrap();
+            std::fs::read_to_string(path).unwrap()
+        }
+    });
+    let result = DecodingKey::from_ec_pem(public_key.as_bytes())?;
     Ok(result)
 }
 
