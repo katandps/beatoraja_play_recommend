f9651bb5cec9899a5e03f7ee4fd0c38d427468b1
katandps <katandps@gmail.com>
2025-01-18 03:24:46 +0900

    add table path env

--- crates/table/src/config.rs
+++ crates/table/src/config.rs
@@ -18,7 +18,8 @@ pub fn config() -> &'static Cfg {
     static INSTANCE: OnceLock<Cfg> = OnceLock::new();
     INSTANCE.get_or_init(|| {
         const DEFAULT_SETTING_PATH: &str = "./tables.toml";
-        let toml = std::fs::read_to_string(DEFAULT_SETTING_PATH).unwrap();
+        let path = std::env::var("TABLES_SETTING_PATH").unwrap_or(DEFAULT_SETTING_PATH.to_string());
+        let toml = std::fs::read_to_string(path).unwrap();
         toml::from_str(&toml).unwrap()
     })
 }
