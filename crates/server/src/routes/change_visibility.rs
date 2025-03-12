97c6f239463526650f8c00b28d20806d62521a04
katandps <katandps@gmail.com>
2025-03-12 16:46:12 +0900

    move change visibility to service

--- crates/server/src/routes/change_visibility.rs
+++ crates/server/src/routes/change_visibility.rs
@@ -1,11 +1,9 @@
-use crate::error::HandleError;
 use crate::filter::{account_by_session, changed_visibility_by_query, with_db};
-use model::Account;
+use crate::json;
 use mysql::MySqlPool;
-use repository::ChangeAccountVisibility;
 use warp::filters::BoxedFilter;
 use warp::path;
-use warp::{Filter, Rejection, Reply};
+use warp::{Filter, Reply};
 
 pub fn route(db_pool: &MySqlPool) -> BoxedFilter<(impl Reply,)> {
     warp::post()
@@ -14,23 +12,7 @@ pub fn route(db_pool: &MySqlPool) -> BoxedFilter<(impl Reply,)> {
         .and(with_db(db_pool))
         .and(account_by_session(db_pool))
         .and(changed_visibility_by_query())
-        .and_then(change_visibility_handler)
+        .then(service::user::change_visibility)
+        .then(json)
         .boxed()
-}
-
-async fn change_visibility_handler<C: ChangeAccountVisibility>(
-    repos: C,
-    mut account: Account,
-    changed_visibility: bool,
-) -> Result<impl Reply, Rejection> {
-    account.set_visibility(changed_visibility);
-    change_visibility(repos, &account).await?;
-    Ok(serde_json::to_string(&account).unwrap())
-}
-
-async fn change_visibility<C: ChangeAccountVisibility>(
-    mut repos: C,
-    account: &Account,
-) -> Result<(), HandleError> {
-    Ok(repos.change_visibility(account).await?)
 }
--- crates/service/src/user.rs
+++ crates/service/src/user.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use model::Account;
-use repository::RenameAccount;
+use repository::{ChangeAccountVisibility, RenameAccount};
 
 use crate::Response;
 
@@ -11,6 +11,19 @@ pub async fn change_name<C: RenameAccount>(
 ) -> Result<Response<Account>> {
     account.set_name(&changed_name);
     let _ = repos.rename(&account).await?;
+    Ok(Response::Ok {
+        tag: None,
+        body: account,
+    })
+}
+
+pub async fn change_visibility<C: ChangeAccountVisibility>(
+    mut repos: C,
+    mut account: Account,
+    changed_visibility: bool,
+) -> Result<Response<Account>> {
+    account.set_visibility(changed_visibility);
+    repos.change_visibility(&account).await?;
     Ok(Response::Ok {
         tag: None,
         body: account,
